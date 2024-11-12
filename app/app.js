b63d0f6930a708b046c3cf3735f64bdb9d1fbb88
Simran <gahrasimranjit@gmail.com>
2024-11-11 18:29:11 -0600

    backend and frontend setuo done

--- app/app.js
+++ app/app.js
@@ -6,8 +6,12 @@ const app = express();
 const port = 3000; 
 
 // body-parser is now built into express!
+
 app.use( express.urlencoded({ extended: true }) ); 
 app.set( "view engine", "ejs" );
+//to use static files like css and js 
+app.use(express.static("public"))
+
 
 // Simple server operation
 app.listen ( port, () => {
@@ -31,5 +35,30 @@ app.post( "/register", ( req, res ) => {
 
 app.post( "/login", ( req, res ) => {
     console.log(req.body)
+    res.redirect("home")
+
+    //here we will be adding the database connection and checking if the user exists in database 
+
+    //after checking we will send ejs file for the homepage
+    res.redirect("home")
     
 });
+
+
+app.get("/home", (req, res) =>{
+
+    res.sendFile(__dirname + "/views/home.html")
+
+
+})
+
+
+
+app.post("/search-result", (req, res) =>{
+
+    console.log(req.body);
+
+    res.sendFile(__dirname + "/views/results.html")
+
+
+})
